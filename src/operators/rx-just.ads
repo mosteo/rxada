38f903ebbd419740347548737ea17a1596d36f08
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-05-26 13:44:42 +0200

    Broken experiments

--- src/operators/rx-just.ads
+++ src/operators/rx-just.ads
@@ -1,6 +1,8 @@
 with Rx.Holder;
 with Rx.Operator;
 
+with Ada.Finalization;
+
 generic
    type T (<>) is private;
    V : T;
@@ -14,8 +16,12 @@ private
 
    package Holder is new Rx.Holder (T);
 
+   type Control (Parent : access Output.Observable'Class) is new Ada.Finalization.Limited_Controlled with null record;
+   overriding procedure Finalize (X : in out Control);
+
    type Observable is new Output.Observable with record
       V : Holder.TH;
+      C : Control (Observable'Access);
    end record;
 
    overriding procedure Subscribe (O : in out Observable;
