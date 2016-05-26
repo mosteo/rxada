bb40966428e83caef14ffd830e2964260da8a556
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-05-26 11:03:16 +0200

    removed test

--- src/rx-examples-threading.adb
+++ src/rx-examples-threading.adb
@@ -1,39 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with System.Address_Image;
 
 with Rx.Debug;
 
 procedure Rx.Examples.Threading is
-
-   procedure Julai (I : Integer) is
-      use System;
-      X : Integer;
-   begin
-      X := I;
-      delay 1.0;
-      Put_Line (X'Img);
-      Put_Line (System.Address_Image (X'Address));
-   end Julai;
-
-   generic
-      Val : Integer;
-      with procedure Blah (I : Integer);
-   package Z is
-      task Go;
-   end Z;
-
-   package body Z is
-      task body Go is
-      begin
-         loop
-            Blah (Val);
-         end loop;
-      end Go;
-   end Z;
-
-   package Z1 is new Z (1, Julai);
-   package Z2 is new Z (2, Julai);
-
 begin
    -- Test;
    Put_Line ("After main");
