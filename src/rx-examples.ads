81084b328d2101016a63277546b20abbc1f96162
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-05-28 16:20:57 +0200

    Complete minimal, but strange error to check with GPL 2015

--- src/rx-examples.ads
+++ src/rx-examples.ads
@@ -1,6 +1,7 @@
-with Rx.Interval;
-with Rx.Schedulers;
+with Rx.Knot;
 
 package Rx.Examples is
+
+   package RxStrings is new Rx.Knot (String); 
 
 end Rx.Examples;
--- src/rx-knot.ads
+++ src/rx-knot.ads
@@ -4,8 +4,22 @@ generic
    type T (<>) is private;
 package Rx.Knot is
 
-   type Observable is new Rx.Producer.Observable with private;
+   package Binding is new Rx.Producer (T);
+
+   type Observable (<>) is new Binding.Observable with private;
 
    function Just (Val : T) return Observable'Class;
+   
+   overriding
+   procedure Subscribe (O : in out Observable; 
+                        S : access Binding.Observer'Class);                        
+                        
+   procedure Subscribe (O : in out Observable;
+                        A : Binding.Action := null);
+   
+private
+
+   type Observable (Real : not null access Binding.Observable'Class) is 
+      new Binding.Observable with null record;
 
 end Rx.Knot;
