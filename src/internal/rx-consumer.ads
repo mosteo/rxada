1cf53c2b41830d1e77c202636b59a6e36b2ed784
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-05-28 11:14:16 +0200

    Towards new design

--- src/internal/rx-consumer.ads
+++ src/internal/rx-consumer.ads
@@ -1,15 +1,13 @@
-
-with Rx.Root;
-
 generic
    type T (<>) is private;
 package Rx.Consumer is
 
    pragma Preelaborate;
 
-   type Observer is abstract new Rx.Root.Observer with null record;
+   type Observer is interface;
 
-   procedure OnNext      (This : in out Observer; V : T) is null;
+   procedure OnNext      (This : in out Observer; V : T) is abstract;
+
    procedure OnCompleted (This : in out Observer) is null;
 
 end Rx.Consumer;
