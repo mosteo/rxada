32b926e4942d8d867dd8f55e33e608c28de5150d
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-05-28 18:52:38 +0200

    It apparently works... going back to the drawing board with this in mind

--- src/rx-examples.ads
+++ src/rx-examples.ads
@@ -2,6 +2,9 @@ with Rx.Knot;
 
 package Rx.Examples is
 
-   package RxStrings is new Rx.Knot (String); 
+   package RxStrings  is new Rx.Knot (String); 
+   package RxIntegers is new Rx.Knot (Integer);
+   
+   package RxStringsToIntegers is new RxStrings.Operators (RxIntegers);
 
 end Rx.Examples;
