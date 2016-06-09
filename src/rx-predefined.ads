a378b6c9cbcb21c61451c44bc27e8411227188d8
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-06-09 13:28:02 +0200

    Unit test setup

---
+++ src/rx-predefined.ads
@@ -0,0 +1,9 @@
+package Rx.Predefined is
+
+--  Instances and default visibility for the common predefined types:
+--  Strings, Integers, StrToInt, IntToInt, IntToStr
+--  TODO: actually implement it
+
+   pragma Pure;
+
+end Rx.Predefined;
