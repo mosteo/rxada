f2caa67676ea9d0bc0d14394c6b4a8dabddd101f
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-06-13 01:27:17 +0200

    Two thread jumps

--- src/main/rx-examples-threading.adb
+++ src/main/rx-examples-threading.adb
@@ -10,12 +10,10 @@ procedure Rx.Examples.Threading is
 begin
    Chain :=
      Integers.Observables.From ((1, 2, 3, 4, 5))
-     &
-     Observe_On (Schedulers.Background)
-     &
-     Map (Image'Access)
-     &
-     Subscribe (Put_Line'Access);
+     & Observe_On (Schedulers.Background)
+     & Map (Image'Access)
+     & Observe_On (Schedulers.IO)
+     & Subscribe (Put_Line'Access);
 exception
    when E : others =>
       Debug.Print (E);
