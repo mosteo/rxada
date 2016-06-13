aea4dc6f086d0ac194809fd1b4cfa3838a0fdfc9
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-06-13 02:00:12 +0200

    There's a memory leak

--- src/main/rx-examples-threading.adb
+++ src/main/rx-examples-threading.adb
@@ -7,13 +7,22 @@ procedure Rx.Examples.Threading is
    use Strings.Observables;
    use IntToStr;
    use StrToInt;
+
+   procedure Wrap is
+   begin
+      for I in 1 .. 100 loop
+         begin
+            Chain :=
+              Integers.Observables.From ((1, 2, 3, 4, 5))
+                --           & Observe_On (Schedulers.Background)
+              & Map (Image'Access)
+              --           & Observe_On (Schedulers.IO)
+              & Subscribe (Put_Line'Access);
+         end;
+      end loop;
+   end Wrap;
 begin
-   Chain :=
-     Integers.Observables.From ((1, 2, 3, 4, 5))
-     & Observe_On (Schedulers.Background)
-     & Map (Image'Access)
-     & Observe_On (Schedulers.IO)
-     & Subscribe (Put_Line'Access);
+   Wrap;
 exception
    when E : others =>
       Debug.Print (E);
