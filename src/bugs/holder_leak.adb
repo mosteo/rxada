24afae9c98032a06bfc0f3feca3ad98f7a9ebe11
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-06-13 11:41:46 +0200

    comments to bug

--- src/bugs/holder_leak.adb
+++ src/bugs/holder_leak.adb
@@ -7,6 +7,7 @@ procedure Holder_Leak is
    package Holders is new Indefinite_Holders (Indef);
    type Def is new Holders.Holder with null record;
 
+   -- This wrapper type is necessary for the leak to manifest
    type Outer is record
       Inner : Def;
    end record;
@@ -14,6 +15,7 @@ procedure Holder_Leak is
    procedure Eat (O : Outer) is
    begin
       for I of O.Inner.Constant_Reference loop
+      -- Using .Element instead there is no leak
          Put_Line (I'Img);
       end loop;
    end Eat;
