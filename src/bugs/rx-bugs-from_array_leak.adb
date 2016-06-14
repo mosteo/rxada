36ffc0fcc0d7d2222280860b7ce490c0f62711d3
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-06-14 18:46:24 +0200

    Empty source, progress (?) with leak bug

--- src/bugs/rx-bugs-from_array_leak.adb
+++ src/bugs/rx-bugs-from_array_leak.adb
@@ -1,4 +1,4 @@
-with Rx.Integers; use Rx.Integers;
+with Rx.Integers; use Rx.Integers; use Rx.Integers.Observables;
 
 procedure Rx.Bugs.From_Array_Leak is
 
@@ -7,12 +7,13 @@ procedure Rx.Bugs.From_Array_Leak is
 begin
    for I in 1 .. 666 loop
       declare
-         Ob  : constant Integers.Observable := Integers.Observables.From ((0, 1, 2, 3));
+         Ob  : constant Integers.Observable := Empty;
          Op  : constant Integers.Observables.Operator := Count (First => 0);
          Ob2 : constant Integers.Observable :=
                  Ob
                  &
-                 Op; -- Leak is around here, but apparently caused by the arrays in From (according to gnatmem)
+                 Op; -- Leak is around here, so caused by the & operator
+
          --  OTOH, valgrind is unable to pinpoint the leak
       begin
          null;
