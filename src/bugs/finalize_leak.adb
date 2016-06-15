879810e50f22de75647d40cb9f2b24575840d2e5
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-06-16 01:50:18 +0200

    As sent to c.l.a

--- src/bugs/finalize_leak.adb
+++ src/bugs/finalize_leak.adb
@@ -7,8 +7,10 @@ procedure Finalize_Leak is
 
       type One is interface;
 
-      type Managed is new Controlled with null record;
-      overriding procedure Finalize (C : in out Managed);
+      type Managed is new Controlled with record
+         X : Integer := 1;
+      end record;
+      overriding procedure Finalize (M : in out Managed);
 
       type Two is new One with record
          M : Managed;
@@ -17,10 +19,11 @@ procedure Finalize_Leak is
    end P;
 
    package body P is
-      overriding procedure Finalize (C : in out Managed) is
-         pragma Unreferenced (C);
+      overriding procedure Finalize (M : in out Managed) is
+         pragma Unreferenced (M);
       begin
-         Put_Line ("finalize");
+         Put_Line ("finalize M");
+         M.X := 0;
       end Finalize;
    end P;
 
@@ -35,25 +38,28 @@ begin
       B : constant One'Class := A;
    begin
       Put_Line ("---8<---");
+      Put_Line (Two'Class (B).M.X'Img);
    end;
    Put_Line ("--->8---");
 
    New_Line;
    declare
---      A : Two;
       B : constant One'Class := Pass (A);
    begin
       Put_Line ("---8<---");
+      Put_Line (Two'Class (B).M.X'Img);
    end;
    Put_Line ("--->8---");
 
    New_Line;
+   A.M.X := 2;
    declare
---      A : Two;
-      B : constant One'Class := not A;
+      B : One'Class := not A;
    begin
       Put_Line ("---8<---");
+      Put_Line (Two'Class (B).M.X'Img);
    end;
    Put_Line ("--->8---");
-   Put_Line ("Now A is going to finalize");
+
+   New_Line; Put_Line ("Now A is going to finalize");
 end Finalize_Leak;
