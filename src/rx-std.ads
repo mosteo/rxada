927f4aa23046c55822e846c1ae7fac5923506837
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-06-22 13:00:13 +0200

    Special sources Error, Throw; confined to Std.

--- src/rx-std.ads
+++ src/rx-std.ads
@@ -1,3 +1,8 @@
+with Ada.Exceptions;
+
+	with Rx.Any;
+	with Rx.Empty;
+	with Rx.Errors;
 	with Rx.Integers;
 private with Rx.Interval;
 	with Rx.Schedulers;
@@ -8,13 +13,29 @@ package Rx.Std is
 --  Strings, Integers, StrToInt, IntToInt, IntToStr
 --  Also default sources/operators from ReactiveX documentation
 
+   package Any renames Rx.Any.Instance;
+
+   function Empty return Any.Observable;
+
+   function Error (E : Rx.Errors.Occurrence)                return Any.Observable;
+   function Error (E : Ada.Exceptions.Exception_Occurrence) return Any.Observable;
+
    function Interval (First       : Integer := 0;
                       Pause       : Duration := 1.0;
                       First_Pause : Duration := 1.0;
                       Scheduler   : Schedulers.Scheduler := Schedulers.Computation)
                       return Integers.Observable;
 
+   function Never return Any.Observable;
+
 private
+
+   package RxEmpty is new Rx.Empty (Any.Typed);
+
+   function Empty return Any.Observable renames RxEmpty.Empty;
+
+   function Error (E : Rx.Errors.Occurrence)                return Any.Observable renames RxEmpty.Error;
+   function Error (E : Ada.Exceptions.Exception_Occurrence) return Any.Observable renames RxEmpty.Error;
 
    package RxInterval is new Rx.Interval (Integers.Typed, Integer'Succ);
 
@@ -23,6 +44,8 @@ private
                       First_Pause : Duration := 1.0;
                       Scheduler   : Schedulers.Scheduler := Schedulers.Computation)
                       return Integers.Observable renames RxInterval.Create;
+
+   function Never return Any.Observable renames RxEmpty.Never;
 
 
 end Rx.Std;
