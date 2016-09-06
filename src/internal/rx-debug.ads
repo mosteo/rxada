a95462feae58f80538bcfd4e85e49ddcaccedef0
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-09-06 11:52:43 +0200

    Debug levels introduced

--- src/internal/rx-debug.ads
+++ src/internal/rx-debug.ads
@@ -9,11 +9,18 @@ package Rx.Debug is
 
 --     pragma Preelaborate;
 
-   Enabled : constant Boolean := True;
+   type Levels is (Verbose, -- Highly chatty
+                   Reduced, -- Out-of-usual
+                   Warning, -- Shouldn't happen but not critical (?)
+                   Erratum  -- Something is definitely not working as expected
+                   );
+
+   Level : constant Levels := Reduced;
+   --  Minimum level a message has to have for it to be printed
+
+   procedure Log (S : String; Level : Levels := Verbose); -- Prints S if above configured level
 
    procedure Print (E : Ada.Exceptions.Exception_Occurrence);
-
-   procedure Log (S : String); -- Prints S if Enabled is true
 
    procedure Put_Line (I : Integer);
    procedure Put_Line (S : String) renames Gnat.IO.Put_Line;
