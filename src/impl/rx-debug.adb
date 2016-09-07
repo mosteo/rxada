e113d9992536a15b5a9ae51706bdb00bebb80d55
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-09-07 11:28:39 +0200

    Subscribe compiles

---
+++ src/impl/rx-debug.adb
@@ -0,0 +1,35 @@
+package body Rx.Debug is
+
+   ---------
+   -- Log --
+   ---------
+
+   procedure Log (S : String; Level : Levels := Verbose) is
+   begin
+      if Level > Debug.Level then
+         Put_Line ("debug: " & S);
+      end if;
+   end Log;
+
+   --------------
+   -- Put_Line --
+   --------------
+
+   procedure Put_Line (I : Integer) is
+   begin
+      Put_Line (I'Img);
+   end Put_Line;
+
+   -----------
+   -- Print --
+   -----------
+
+   procedure Print (E : Ada.Exceptions.Exception_Occurrence) is
+   begin
+      Put_Line ("Uh oh...");
+      Put_Line (Ada.Exceptions.Exception_Name (E));
+      Put_Line (Ada.Exceptions.Exception_Message (E));
+      Put_Line (Ada.Exceptions.Exception_Information (E));
+   end Print;
+
+end Rx.Debug;
--- src/rx-subscribe.ads
+++ src/rx-subscribe.ads
@@ -5,8 +5,10 @@ generic
    with package Typed is new Rx.Typed (<>);
 package Rx.Subscribe is
 
+   pragma Preelaborate;
+
    function Create (On_Next      : Typed.Actions.Proc1   := null;
                     On_Completed : Rx.Actions.Proc0      := null;
-                    On_Error     : Rx.Actions.Proc_Error := null) return Typed.Producers.Subscriptor'Class;
+                    On_Error     : Rx.Actions.Proc_Error := null) return Typed.Contracts.Sink'Class;
 
 end Rx.Subscribe;
