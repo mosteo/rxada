2907d35f7f1a6ad70a1931503d8339e8dedfc9f9
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-09-19 15:14:32 +0200

    Added trace on all unhandled exceptions

--- src/main/rx-examples-tests.adb
+++ src/main/rx-examples-tests.adb
@@ -1,7 +1,11 @@
+with GNAT.Exception_Traces;
+
 with Rx.Tests;
 
 procedure Rx.Examples.Tests is
 begin
+   GNAT.Exception_Traces.Trace_On (GNAT.Exception_Traces.Unhandled_Raise);
+
    pragma Assert (Rx.Tests.Basic_Tests);
    pragma Assert (Rx.Tests.No_Op);
    pragma Assert (Rx.Tests.Subscriptions);
