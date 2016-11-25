3fb3be94a3e3816102ef363fb343ec04def118c0
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-11-25 14:50:06 +0100

    simpler iterable traits

--- src/traits/rx-traits-iterable.ads
+++ src/traits/rx-traits-iterable.ads
@@ -1,15 +1,12 @@
 with Rx.Typed;
 
 pragma Warnings (Off);
+
 generic
    with package Typed is new Rx.Typed (<>);
-
-   type Container is limited private;
-   type Cursor is private;
-   with function First (C : Container) return Cursor is <>;
-   with function Next (C : Cursor) return Cursor is <>;
-   with function Element (C : Cursor) return Typed.Type_Traits.T is <>;
-   with function Has_Element (C : Cursor) return Boolean is <>;
+   type Container is private;
+   with procedure Iterate (C : Container; Proc : access procedure (V : Typed.T)) is <>;
+   --  Iterate should call its second parameter procedure for each element in the container
 package Rx.Traits.Iterable with Preelaborate is
 
 end Rx.Traits.Iterable;
