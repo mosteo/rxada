141dfbbdfb97f643a6951c473bf9ac4e439d5647
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-11-30 19:00:29 +0100

    s/Integer/Rx_Integer/

--- src/impl/rx-impl-casts.ads
+++ src/impl/rx-impl-casts.ads
@@ -5,13 +5,13 @@ package Rx.Impl.Casts with Preelaborate is
 
    --  Casts between the standard Ada types for use in the Cast operator
 
-   function To_Integer (V : Float)  return Integer is (Integer (V));
-   function To_Integer (V : String) return Integer is (Integer'Value (V));
+   function To_Integer (V : Float)  return Rx_Integer is (Rx_Integer (V));
+   function To_Integer (V : String) return Rx_Integer is (Rx_Integer'Value (V));
 
-   function To_Float (V : Integer) return Float is (Float (V));
+   function To_Float (V : Rx_Integer) return Float is (Float (V));
    function To_Float (V : String)  return Float is (Float'Value (V));
 
-   function To_String (V : Integer) return String is (Trim (Integer'Image (V), Both));
+   function To_String (V : Rx_Integer) return String is (Trim (Rx_Integer'Image (V), Both));
    function To_String (V : Float)   return String is (Trim (Float'Image (V), Both));
 
 end Rx.Impl.Casts;
--- src/impl/rx.ads
+++ src/impl/rx.ads
@@ -7,7 +7,10 @@ package Rx with Pure is
    type    Rx_Any      is interface;
    --  By deriving user types from Rx_Any, users can benefit from some Operators in Rx.Std by default
 
-   subtype Rx_Integer  is Integer;
+   --  The following are defaults for the default operators.
+   --  For specific need the user can create instances for any desired type.
+
+   subtype Rx_Integer  is Long_Long_Integer;
    subtype Rx_Natural  is Rx_Integer range 0 .. Rx_Integer'Last;
    subtype Rx_Positive is Rx_Integer range 1 .. Rx_Integer'Last;
 
