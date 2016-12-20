f77e96b8d35f1c38f514944c81cf8f2457b07889
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-12-20 16:04:20 +0100

    Initial localized refactoring completed

--- src/body/rx-transformers.adb
+++ src/body/rx-transformers.adb
@@ -3,6 +3,18 @@ with Rx.Subscriptions;
 
 package body Rx.Transformers is
 
+   -----------
+   -- Clear --
+   -----------
+
+   procedure Clear (This : in out Transformer'Class) is
+   begin
+      if This.Operator.Is_Valid then
+         This.Get_Operator.Subscriber.Clear;
+         This.Operator.Clear;
+      end if;
+   end Clear;
+
    ---------------
    -- Subscribe --
    ---------------
@@ -11,13 +23,12 @@ package body Rx.Transformers is
      (Producer : in out Transformer;
       Consumer : in out Into.Subscriber)
    is
---      use type Into.Consumers.Holder;
    begin
       if Producer.Has_Parent then
          declare
             Parent : From.Observable := Producer.Get_Parent; -- Our own copy
          begin
-            Producer.Set_Child (Consumer); -- With its own child
+            Producer.Get_Operator.Set_Subscriber (Consumer);
             Parent.Subscribe (Producer);
          end;
       else
@@ -31,15 +42,15 @@ package body Rx.Transformers is
 
    overriding procedure On_Next (This : in out Transformer; V : From.T) is
    begin
-      if This.Has_Child then
-         Transformer'Class (This).On_Next (V, This.Get_Child);
+      if This.Operator.Is_Valid then
+         This.Get_Operator.On_Next (V);
       else
          raise Subscriptions.No_Longer_Subscribed;
       end if;
    exception
       when Subscriptions.No_Longer_Subscribed =>
          Debug.Log ("Transform.On_Next: caught No_Longer_Subscribed", Debug.Note);
-         This.Child.Clear;
+         This.Clear;
          raise;
    end On_Next;
 
@@ -49,17 +60,18 @@ package body Rx.Transformers is
 
    overriding procedure On_Completed (This : in out Transformer) is
    begin
-      if This.Has_Child then
-         Transformer'Class (This).On_Completed (This.Get_Child);
-         This.Child.Clear; -- This implies unsubscription, but does not propagate it down
+      if This.Operator.Is_Valid then
+         begin
+            This.Get_Operator.On_Completed;
+            This.Clear;
+         exception
+            when others =>
+               This.Clear;
+               raise;
+         end;
       else
          raise Subscriptions.No_Longer_Subscribed;
       end if;
-   exception
-      when Subscriptions.No_Longer_Subscribed =>
-         Debug.Log ("Transform.On_Completed: caught No_Longer_Subscribed", Debug.Note);
-         This.Child.Clear;
-         raise;
    end On_Completed;
 
    --------------
@@ -68,48 +80,18 @@ package body Rx.Transformers is
 
    overriding procedure On_Error (This : in out Transformer; Error : Errors.Occurrence) is
    begin
-      if This.Has_Child then
-         declare
-            Err : Errors.Occurrence := Error; -- Writable copy, until I fix this mess
+      if This.Operator.Is_Valid then
          begin
-            Transformer'Class (This).On_Error (Err, This.Get_Child); -- Pass it down
-            This.Child.Clear;
+            This.Get_Operator.On_Error (Error);
+            This.Clear;
          exception
-            when Subscriptions.No_Longer_Subscribed =>
-               Debug.Log ("Transform.On_Error: caught No_Longer_Subscribed", Debug.Note);
-               This.Child.Clear;
+            when others =>
+               This.Clear;
                raise;
          end;
       else
          Error.Reraise;
       end if;
-   end On_Error;
-
-   --  Versions for the descendants to override:
-
-   ------------------
-   -- On_Completed --
-   ------------------
-
-   procedure On_Completed (This : in out Transformer;
-                           Child : in out Into.Observer'Class)
-   is
-      pragma Unreferenced (This);
-   begin
-      Child.On_Completed;
-   end On_Completed;
-
-   --------------
-   -- On_Error --
-   --------------
-
-   procedure On_Error (This  : in out Transformer;
-                       Error : in out Errors.Occurrence;
-                       Child : in out Into.Observer'Class)
-   is
-      pragma Unreferenced (This);
-   begin
-      Child.On_Error  (Error);
    end On_Error;
 
    -------------------
@@ -119,20 +101,30 @@ package body Rx.Transformers is
    overriding
    procedure Unsubscribe (This : in out Transformer) is
    begin
-      if This.Is_Subscribed then
-         This.Get_Child.Unsubscribe;
-         This.Child.Clear;
+      if This.Operator.Is_Valid then
+         This.Get_Operator.Unsubscribe;
+         This.Clear;
       end if;
+   exception
+      when Subscriptions.No_Longer_Subscribed =>
+         Debug.Log ("Transform.Unsubscribe: caught No_Longer_Subscribed", Debug.Note);
+         This.Clear;
+      when others =>
+         This.Clear;
+         raise;
    end Unsubscribe;
 
-   ---------------
-   -- Set_Child --
-   ---------------
+   -----------------
+   -- Unsubscribe --
+   -----------------
 
-   procedure Set_Child (This : in out Transformer; Child : Into.Subscriber) is
+   overriding procedure Unsubscribe (This : in out New_Operator) is
    begin
-      This.Child.Hold (Child);
-   end Set_Child;
+      if This.Is_Subscribed then
+         This.Get_Subscriber.Unsubscribe;
+      end if;
+      This.Subscriber.Clear;
+   end Unsubscribe;
 
    ------------------
    -- Will_Observe --
@@ -154,7 +146,7 @@ package body Rx.Transformers is
 
    overriding procedure On_Completed (This : in out New_Operator) is
    begin
-      This.Get_Observer.On_Completed;
+      This.Get_Subscriber.On_Completed;
    end On_Completed;
 
    --------------
@@ -163,25 +155,16 @@ package body Rx.Transformers is
 
    overriding procedure On_Error (This : in out New_Operator; Error : Errors.Occurrence) is
    begin
-      This.Get_Observer.On_Error (Error);
+      This.Get_Subscriber.On_Error (Error);
    end On_Error;
 
    ------------------
    -- Set_Observer --
    ------------------
 
-   not overriding procedure Set_Observer (This : in out New_Operator; Observer : Into.Subscriber'Class) is
+   not overriding procedure Set_Subscriber (This : in out New_Operator; Observer : Into.Subscriber'Class) is
    begin
-      This.Observer.Hold (Observer);
-   end Set_Observer;
-
-   ------------------
-   -- Get_Observer --
-   ------------------
-
-   function Get_Observer (This : in out New_Operator'Class) return Into.Holders.Subscribers.Reference is
-   begin
-      return This.Observer.Ref;
-   end Get_Observer;
+      This.Subscriber.Hold (Observer);
+   end Set_Subscriber;
 
 end Rx.Transformers;
