33616cb74dde3d1f1efa39d548a4ed226331b37b
Alejandro R Mosteo <264377+mosteo@users.noreply.github.com>
2016-12-20 16:04:20 +0100

    Initial localized refactoring completed

--- src/impl/rx-transformers.ads
+++ src/impl/rx-transformers.ads
@@ -21,11 +21,7 @@ package Rx.Transformers is
    package Actions is new Rx.Actions.Transform (From.Contracts, Into.Contracts);
 
    --  Transformative operator scaffolding:
-
    package Links is new Rx.Impl.Links (From);
-
-   package Child_Holders is new Rx.Holders (Into.Subscriber'Class, "transform.observer'class");
-   type Child_Holder is new Child_Holders.Definite with null record;
 
    type Transformer (<>) is new
      Links.Downstream and
@@ -34,13 +30,21 @@ package Rx.Transformers is
    with private;
    --  This is the fundamental type that bridges observables y observers doing something along the way
    --  For simpler operator creation, see the Operator type below
+   --  A Transformer wraps an operator taking care of checks that are common to most operators
 
-   subtype Operator is Transformer'Class;
-
-   type New_Operator is abstract new From.Contracts.Observer with private;
+   type New_Operator is abstract new From.Contracts.Subscriber with private;
    --  This is the type recommended to override for implementing new operators
    --  In conjunction with the Create function here, it provides a proper transformer
    --  with correct management of subscriptions and errors, and proper defaults
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create (Using : New_Operator'Class) return Transformer'Class;
+
+
+   --  Defaults to be overriden per operator
 
    overriding procedure On_Next (This : in out New_Operator; V : From.T) is null;
    --  Just drops the value
@@ -51,55 +55,40 @@ package Rx.Transformers is
    overriding procedure On_Error (This : in out New_Operator; Error : Errors.Occurrence);
    --  Just passes it along
 
-   not overriding procedure Set_Observer (This : in out New_Operator; Observer : Into.Subscriber'Class);
+   not overriding procedure Set_Subscriber (This : in out New_Operator; Observer : Into.Subscriber'Class);
    --  Override only if you need a modified observer (for example a Shared one)
 
-   function Get_Observer (This : in out New_Operator'Class) return Into.Holders.Subscribers.Reference;
+
+   --  From this point on there should be no need to override the rest of methods
+
+   function Get_Subscriber (This : in out New_Operator'Class) return Into.Holders.Subscribers.Reference;
    --  Use this in order to get the next observer in chain
 
-   --  To have common code not lost, new operators should extend this one, leaving the original
-   --  Consumer interface intact (On_Next, etc). Instead, these versions that receive the Observer
-   --  as parameter should be overriden.
+   overriding function Is_Subscribed (This : New_Operator) return Boolean;
+   --  Proper default, no need to override
 
-   function Create (Using : New_Operator'Class) return Transformer'Class;
+   overriding procedure Unsubscribe (This : in out New_Operator);
+   --  Proper default, no need to override
 
-   procedure On_Next (This  : in out Transformer;
-                      V     :        From.T;
-                      Child : in out Into.Observer) is null;
-   pragma Deprecated;
-   --  Must always be provided
+   --  There should be no need to override the following methods
 
-   procedure On_Completed (This  : in out Transformer;
-                           Child : in out Into.Observer);
-   --  By default calls Child.On_Complete
+   function Will_Observe (Producer : From.Observable;
+                          Consumer : Transformer'Class)
+                          return Into.Observable;
+   --  This does the magic of preparing a passive chain, ready for actual subscription/observation
 
-   procedure On_Error (This  : in out Transformer;
-                       Error : in out Errors.Occurrence;
-                       Child : in out Into.Observer);
-   --  By default calls Child.On_error
+   function "&" (Producer : From.Observable;
+                 Consumer : Transformer'Class)
+                 return Into.Observable renames Will_Observe;
 
-   --  FOLLOWING CAN BE OVERRIDEN IF DEFAULT BEHAVIOR HAS TO BE MODIFIED, BUT THESE ARE PROPER DEFAULTS
-
-   overriding
-   function Is_Subscribed (This : Transformer) return Boolean;
+   --  Other overridings that can be left as-is
 
    overriding
    procedure Subscribe (Producer : in out Transformer;
                         Consumer : in out Into.Subscriber);
 
-   procedure Set_Child (This : in out Transformer; Child : Into.Subscriber);
-   -- Can be used to override the default "&" behavior
-
-   not overriding function Get_Child (This : in out Transformer)
-                                      return Child_Holders.Reference;
-
    overriding
    procedure Unsubscribe (This : in out Transformer);
-   --  Once the child is no longer needed let it gooo!
-
-
-   --  DO NOT OVERRIDE THE STANDARD FOLLOWING METHODS IN NEW OPERATORS
-   --  OR ELSE CALL THE PARENT IMPLEMENTATIONS
 
    overriding
    procedure On_Next (This : in out Transformer; V : From.T);
@@ -113,19 +102,13 @@ package Rx.Transformers is
    procedure On_Error (This : in out Transformer; Error : Errors.Occurrence);
    --  By default calls downstream On_Error
 
-   function Will_Observe (Producer : From.Observable;
-                          Consumer : Transformer'Class)
-                          return Into.Observable;
-   --  This does the magic of preparing a passive chain, ready for actual subscription/observation
-
-   function "&" (Producer : From.Observable;
-                 Consumer : Transformer'Class)
-                 return Into.Observable renames Will_Observe;
+   overriding
+   function Is_Subscribed (This : Transformer) return Boolean;
 
 private
 
-   type New_Operator is abstract new From.Contracts.Observer with record
-      Observer : Into.Holders.Subscriber;
+   type New_Operator is abstract new From.Contracts.Subscriber with record
+      Subscriber : Into.Holders.Subscriber;
    end record;
 
    package Operator_Holders is new Rx.Holders (New_Operator'Class);
@@ -135,21 +118,25 @@ private
      Into.Contracts.Observable and
      From.Contracts.Subscriber
    with record
-      Child : Child_Holder;
-
-      Managed : Operator_Holders.Definite;
+      Operator : Operator_Holders.Definite;
    end record;
 
-   not overriding function Has_Child (This : Transformer) return Boolean is (not This.Child.Is_Empty);
+   overriding function Is_Subscribed (This : Transformer) return Boolean is
+     (This.Operator.Is_Valid and then This.Operator.CRef.Is_Subscribed);
 
-   not overriding function Get_Child (This : in out Transformer)
-                                      return Child_Holders.Reference is (This.Child.Ref);
-
-   overriding function Is_Subscribed (This : Transformer) return Boolean is (not This.Child.Is_Empty);
+   overriding function Is_Subscribed (This : New_Operator) return Boolean is (This.Subscriber.Is_Valid);
 
    function Create (Using : New_Operator'Class) return Transformer'Class is
      (Transformer'(Links.Downstream with
-                   Managed => Operator_Holders.Hold (Using),
-                   others  => <>));
+                   Operator => Operator_Holders.Hold (Using)));
+
+   function Get_Subscriber (This : in out New_Operator'Class) return Into.Holders.Subscribers.Reference is
+     (This.Subscriber.Ref);
+
+   function Get_Operator (This : in out Transformer'Class) return Operator_Holders.Reference is
+      (This.Operator.Ref);
+
+   procedure Clear (This : in out Transformer'Class);
+   --  Dispose of as much as possible
 
 end Rx.Transformers;
