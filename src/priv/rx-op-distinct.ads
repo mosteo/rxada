dc97f2556f9e8e5613d0ef412540eb457d196f87
Alejandro R. Mosteo <alejandro@mosteo.com>
2017-01-12 14:33:57 +0100

    Element_At

--- src/priv/rx-op-distinct.ads
+++ src/priv/rx-op-distinct.ads
@@ -6,7 +6,7 @@ package Rx.Op.Distinct is
 
    function Default_Not_Same (L, R : Operate.T) return Boolean;
 
-   function Create (Is_Distinct : Operate.Typed.Actions.Comparator := Default_Not_Same'Access)
+   function Create (Are_Distinct : Operate.Typed.Actions.Comparator := Default_Not_Same'Access)
                     return Operate.Operator'Class;
    --  When null, default "=" is used to compare elements
 
